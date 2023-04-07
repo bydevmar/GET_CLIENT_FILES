15fae44d88f87a3e3f9a12d2680ac517d3ba2581
DevLop99 <83462650+DevLop99@users.noreply.github.com>
2023-04-07 14:15:24 +0200

    CREATE "ENTREPRISES_EXERCICES" MODEL
    CREATE "ENTREPRISES_EXERCICES" MODEL

---
+++ SERVER/models/Entreprise_Exercice.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+    const ENTREPRISES_EXERCICES = sequelize.define("ENTREPRISES_EXERCICES", {
+        e_e_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        entreprise_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'ENTREPRISES',
+                key: 'entreprise_id'
+            }
+        },
+        exercice_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'EXERCICES',
+                key: 'exercice_id'
+            }
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        deletedAt: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: null
+        }
+    });
+    return ENTREPRISES_EXERCICES;
+}
+
