c7dc40ffc311b55d39f76654f9b1782f7cfe1261
DevLop99 <83462650+DevLop99@users.noreply.github.com>
2023-04-07 14:07:54 +0200

    CREATE "ENTREPRISES" MODEL
    CREATE "ENTREPRISES" MODEL

---
+++ SERVER/models/Entreprises.js
@@ -0,0 +1,65 @@
+module.exports = (sequelize, DataTypes) => {
+    const ENTREPRISES = sequelize.define("ENTREPRISES", {
+        entreprise_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nom_entreprise: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        telephone_entreprise: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email_entreprise: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        //
+        emplacement_entreprise: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "adresses",
+                key: 'adresse_id',
+            },
+        },
+        Industrie_entreprise: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        //Ce serait le chiffre d'affaires annuel ou le revenu de l'entreprise.
+        Revenu: {
+            type: DataTypes.FLOAT,
+            allowNull: true
+        },
+        proprietaire_entreprise: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "CLIENTS",
+                key: 'client_id',
+            },
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        deletedAt: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: null
+        }
+    });
+    return ENTREPRISES;
+}
+
