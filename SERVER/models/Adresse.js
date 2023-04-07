7228078f24c0ce9d9122b37f94468dc654fd13ad
DevLop99 <83462650+DevLop99@users.noreply.github.com>
2023-04-07 13:00:15 +0200

    CREATE " ADRESSE " MODEL

---
+++ SERVER/models/Adresse.js
@@ -0,0 +1,49 @@
+
+
+module.exports = (sequelize, DataTypes) => {
+    const ADRESSES = sequelize.define('ADRESSES', {
+      adresse_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      adresse : {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      ville: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      codepostal : {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      pays : {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    }, {
+      timestamps: true,
+      paranoid: true,
+      underscored: true,
+      freezeTableName: true,
+    });
+  
+    return ADRESSES;
+  };
+  
